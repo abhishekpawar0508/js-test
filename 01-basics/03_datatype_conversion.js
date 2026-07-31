df9a3074a7f2f805aa437bed34d20d251a681115
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-07-31 07:24:46 +0000

    data conversion

---
+++ 01-basics/03_datatype_conversion.js
@@ -0,0 +1,30 @@
+let score = "";
+
+let numScore = Number(score)
+
+// console.log(typeof score)
+// console.log(typeof numScore);
+// console.log(numScore);
+
+// "33" => 33
+// "33abc" => NaN
+// null => object
+// boolean => 1/0
+// undefined => NaN
+
+let isLoggedIn = 2;
+
+let checkLoggedIn = Boolean(isLoggedIn)
+// console.log(checkLoggedIn);
+
+// number => true
+// 0 => false
+// string => true
+// "" (empty string) => false
+
+let age = 33
+
+let chagneToAgeNumber = String(age)
+console.log(chagneToAgeNumber);
+console.log(typeof chagneToAgeNumber);
+
