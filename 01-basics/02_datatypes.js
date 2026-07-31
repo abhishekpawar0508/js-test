3623ee95e2f50a0a6f8bf65f4cb4d69e339f8fff
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-07-31 01:39:38 +0000

    variable and datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all code as per new standard
+
+let userName = "Abhi"
+let userId = 345
+let isLoggedIn = true
+let userAge = null
+
+// *primitive data types
+// number => 2 to pwer 53
+// bigInt => for large number of values, mostly used in stock/trading websites
+//string => ""
+//boolean => true/false
+// null => standalone value (empty value when you not required the answer 0, like temperature)
+//undefined => value is not defined yet
+//symbol => used mostly for identify unique like figma use of component
+
+// *non-premitive data type
+//object
+
+console.log(typeof null); // type of null is object
+console.log(typeof undefined); // type of undefined is undefined
+
