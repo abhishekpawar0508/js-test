77db31b6722d6fcd25f3b455ad5e1aa4a336edcc
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-07-31 10:41:16 +0000

    operators

--- 01-basics/03_datatype_conversion.js
+++ 01-basics/03_datatype_conversion.js
@@ -25,6 +25,30 @@ let checkLoggedIn = Boolean(isLoggedIn)
 let age = 33
 
 let chagneToAgeNumber = String(age)
-console.log(chagneToAgeNumber);
-console.log(typeof chagneToAgeNumber);
+// console.log(chagneToAgeNumber);
+// console.log(typeof chagneToAgeNumber);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// if number is on first place it adds first adn if string is on first place it treat as string
+
+let num1, num2, num3
+num1 = num2 = num3 = 4 + 4
+
+// console.log(num1, num2, num3)
+
+
+let matchScore = 200
+matchScore++;
+
+let teamScore = 100
+++teamScore
+
+console.log(matchScore);
+console.log(teamScore);
+
+console.log(matchScore + teamScore);
+
+
 
