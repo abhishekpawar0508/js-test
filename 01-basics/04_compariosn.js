faf58fbbd122e5f22ae0db0a05ae95b27c9a6e94
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-07-31 12:13:56 +0000

    comparison

---
+++ 01-basics/04_compariosn.js
@@ -0,0 +1,22 @@
+// console.log("2" == 2);
+// console.log(2 > "3");
+// console.log(2 == "2");
+
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null < 0);
+
+// console.log(undefined == 0)
+// console.log(undefined > 0);
+// console.log(undefined < 0);     
+
+console.log(2 === "2");
+console.log(null ===  0);
+
+
+
+// we have to write clean and understandable code
+// avoid compare different data type
+
+
+
