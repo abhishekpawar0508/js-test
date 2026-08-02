e3801cc70ca4be8f179998c832102f3b486d157a
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-08-02 13:57:46 +0000

    string methods

---
+++ 01-basics/05_string.js
@@ -0,0 +1,74 @@
+let userFirstName = "Abhi "
+let userLastName = "Kumar"
+
+// console.log(userFirstName + userLastName);
+
+//don't use this method, it's old way to write code
+
+//Always use string interpolation, which is modern way to write code
+
+// console.log(`My name first name is ${userFirstName} and my last name is ${userLastName}`)
+
+let userName = new String("Captain America")
+
+// console.log(userName.__proto__)
+// this means it's an object
+
+// console.log(userName[3])
+// console.log(userName.length);
+
+let userEmail = "  abc.google.com   "
+// console.log(userEmail);
+// console.log(userEmail.trim());
+
+// console.log(userName.slice(2, 5))
+// console.log(userName.split(1));
+
+// console.log(userName.toUpperCase());
+// console.log(userName.toLowerCase());
+// console.log(userName.indexOf("t"));
+// console.log(userName.charAt(2));
+// console.log(userName.at(-2));
+// "at" character at for both positon positive/negative
+
+// console.log(userName.indexOf("a"));
+// console.log(userName.includes("a"));
+// console.log(userName.includes("Z"))
+
+// console.log(userName[3]);
+// console.log(userName.startsWith(""));
+// console.log(userName.startsWith("Captain", 0));
+// console.log(userName.startsWith("America", 8));
+
+// console.log(userName.endsWith("America"));
+// console.log(userName.length);
+// console.log(userName.endsWith("ca", 15));
+// console.log(userName.replace("a", "b"));
+
+// console.log(userName.toLowerCase().replaceAll("a", "b"));
+// we can use two or more methods like this
+
+// console.log(userName.concat(userName.substring(2, 5)))
+// console.log(userName.substring(2, 5))
+
+// console.log(userName.repeat(3)); 
+// if repeat is 0 is return empty string
+
+// console.log(userName.match("n A"));
+// console.log(userEmail.match(/.com /));
+// console.log(userName.normalize());
+
+
+// console.log(userName.slice(2, 4));
+// console.log(userName.substring(2, 4));
+
+// console.log(userName.split(""));
+
+let file = "photo.jpg"
+// console.log(file.slice(file.lastIndexOf(".")));
+// console.log(userName.slice(8, 3))
+// console.log(userName.substring(8, 3))
+
+// console.log(userName.padStart(18, "*"));
+// console.log(userName.padEnd(18, "*"));
+
