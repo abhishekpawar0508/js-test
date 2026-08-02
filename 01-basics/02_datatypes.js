5e3cf85f0d218463711d0be4de503a31d406c519
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-08-02 00:31:30 +0000

    datatypes

--- 01-basics/02_datatypes.js
+++ 01-basics/02_datatypes.js
@@ -4,6 +4,13 @@ let userName = "Abhi"
 let userId = 345
 let isLoggedIn = true
 let userAge = null
+let bigNumber = 1242343435345n
+
+let consumerId = Symbol("1234");
+let anotherUserId = Symbol("1234")
+
+// console.log(userId === anotherUserId);
+
 
 // *primitive data types
 // number => 2 to pwer 53
@@ -14,9 +21,28 @@ let userAge = null
 //undefined => value is not defined yet
 //symbol => used mostly for identify unique like figma use of component
 
-// *non-premitive data type
-//object
+// *non-premitive data type (Reference types)
+//object, array
 
-console.log(typeof null); // type of null is object
-console.log(typeof undefined); // type of undefined is undefined
+// console.log(typeof null); // type of null is object
+// console.log(typeof undefined); // type of undefined is undefined
 
+let additionFunction = function myFunction(){
+
+}
+
+// console.log(typeof additionFunction); 
+//datatype function but its known as function object
+
+let userDetails = {
+    name: "Abc",
+    age: 25
+}
+
+// console.log(typeof userDetails);
+
+// console.log(typeof bigNumber);
+
+// let marvel = ["Spiderman", "Iron Man", "Doctor Strange"]
+
+// console.log(typeof marvel);
