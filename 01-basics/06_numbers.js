eb012ab37da7003981e3d35e95e8ba6499b229a7
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-08-04 01:30:00 +0000

    numbers

---
+++ 01-basics/06_numbers.js
@@ -0,0 +1,42 @@
+const score = 400;
+// console.log(score);
+
+const teamScore = new Number(100)
+// console.log(teamScore);
+
+// console.log(teamScore.toString().length)
+// console.log(typeof teamScore);
+// console.log(teamScore.toFixed(2))
+
+const otherScore = 400.835;
+// console.log(otherScore.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************** Maths ************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// absolute value(abs) used to convert only negative number to positive
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(2, 3));
+// console.log(Math.min(4, 2, 8, 6));
+// console.log(Math.max(4, 2, 8, 6));
+// console.log(Math.random());
+// Random value will be always between 0-1 (0.255, 0.45484)
+
+// console.log(Math.random()*10 +1);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 20;
+const max = 40;
+
+// console.log(Math.floor(Math.random() * (max -min + 1)) + min);
+
+
