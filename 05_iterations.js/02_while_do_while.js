1eef64a458ce1cc73cc81bac39be673c49c19725
abhishekpawar0508 <talk2abhishekpawar@gmail.com>
2026-08-18 01:39:33 +0000

    while

---
+++ 05_iterations.js/02_while_do_while.js
@@ -0,0 +1,30 @@
+
+let  i = 1;
+while (i <= 10) {
+    // console.log(`${i}`);
+        i = i + 2
+}
+
+let heroes = ["Batman", "Superman", "thor", "Hulk"]
+let arr = 0
+while(arr < heroes.length) {
+    // console.log(`My Super heroes ${heroes[arr]}`);
+    arr = arr + 1;
+}
+
+
+let score = 1
+
+do {
+    // console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
+
+
+let scoreNew = 11
+
+do {
+    console.log(`score is ${scoreNew}`);
+    scoreNew ++
+} while (scoreNew <= 10);
+/* we are checking condition at last that's why do while loop run ones */
